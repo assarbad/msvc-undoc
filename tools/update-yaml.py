4ebd233a784bb017e492317f946839c58b409e31
Oliver Schneider <oliver@assarbad.net>
2023-07-03 23:27:36 +0200

    Minor adjustment to align with my cookiecutter changes

--- tools/update-yaml.py
+++ tools/update-yaml.py
@@ -33,38 +33,34 @@ eprint = partial(print, file=sys.stderr)
 if not sys.version_info[:2] in __compatible__:
     sys.exit(f"This script is only compatible with the following Python versions: {', '.join([f'{z[0]}.{z[1]}' for z in __compatible__])}")  # pragma: no cover
 
+CONFIG_DEFAULTS = """
+[msvc.yaml]
+path = %(thisdir)s/../msvc.yaml
+
+[help-output]
+path = %(thisdir)s/../help-output
+
+[mirrors]
+cpp-docs = %(thisdir)s/mirrors/cpp-docs
+geoffchappell.com = %(thisdir)s/mirrors/msvc
+"""
+
 
 def parse_options() -> argparse.Namespace:
     """\
-        Initializes the argument parser and performs the parsing
+        Initializes the ArgumentParser and ConfigParser and performs the parsing
     """
     from argparse import ArgumentParser
     from configparser import ConfigParser
     from textwrap import dedent
 
-    thisdir = Path(__file__).absolute().parent
-    ininame = thisdir / "msvc-undoc.ini"
-    cfg = ConfigParser(defaults={"thisdir": thisdir}, delimiters=("=",))
-    cfg.read_string(
-        dedent(
-            f"""
-            [msvc.yaml]
-            path = {thisdir}/../msvc.yaml
-
-            [help-output]
-            path = {thisdir}/../help-output
-
-            [mirrors]
-            cpp-docs = {thisdir}/mirrors/cpp-docs
-            geoffchappell.com = {thisdir}/mirrors/msvc
-            """
-        ),
-        "<DEFAULTS>",
-    )
+    cfgname = Path(__file__).absolute().parent / "msvc-undoc.ini"
+    cfg = ConfigParser(defaults={"thisdir": cfgname.parent}, delimiters=("=",))
+    cfg.read_string(dedent(CONFIG_DEFAULTS), "<DEFAULTS>")
 
     parser = ArgumentParser(description="Update YAML with command line switch descriptions", add_help=False)
     # Only add the configuration file argument for starters
-    parser.add_argument("-c", "--config", "--ini", action="store", default=ininame, metavar="CFG", type=Path, help=f"The config file; defaults to {ininame}")
+    parser.add_argument("-c", "--config", "--ini", action="store", default=cfgname, metavar="CFG", type=Path, help=f"The config file; defaults to {cfgname}")
     partial_args = parser.parse_known_args()[0]
     cfg.read(partial_args.config)
 
@@ -76,8 +72,8 @@ def parse_options() -> argparse.Namespace:
     parser.add_argument("--msvc", action="store", default=cfg.get("mirrors", "geoffchappell.com"), type=Path, help=argparse.SUPPRESS)
     parser.add_argument("--cppdocs", "--cpp-docs", action="store", default=cfg.get("mirrors", "cpp-docs"), type=Path, help=argparse.SUPPRESS)
     parser.add_argument("--helpoutput", "--help-output", action="store", default=cfg.get("help-output", "path"), type=Path, help=argparse.SUPPRESS)
-    parser.add_argument("-v", "--verbose", action="count", default=0, help="Turn up verbosity to see more details of what is going on.")  # noqa: E501
-    return parser.parse_args()
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="Turn up verbosity to see more details of what is going on.")
+    return cfg, parser.parse_args()
 
 
 @cache
@@ -295,7 +291,8 @@ def main() -> int:
 
 if __name__ == "__main__":
     global args
-    args = parse_options()
+    global cfg
+    cfg, args = parse_options()
     try:
         sys.exit(main(**vars(args)))
     except SystemExit:
